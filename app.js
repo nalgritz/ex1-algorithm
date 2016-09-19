762f4fe0f8a31c6ea7fb79acf8872ad4ab362307
nalgritz <nalgritz9270@gmail.com>
2016-09-19 22:36:31 +0800

    anagram test done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function checkAnagrams(phrase1, phrase2) {
   function setForCompare (str) {
     return str.toLowerCase().split('').sort().join('')
   };
-  return setForCompare(phrase1) == setForCompare(phrase2);
+  return setForCompare(phrase1) === setForCompare(phrase2);
 };
 
 checkAnagrams("Listen", "silent");
